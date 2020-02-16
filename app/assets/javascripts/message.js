8db53b516b5d5d7df1b8a0b9b2381c83253d26b8
yasuhisa <yasuhisa3264@example.com>
2020-02-17 08:26:44 +0900

    消えたあと

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if(message.image){
+     let html =
+     `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+              <div class="message__user__name">
+                ${message.user_name}
+              </div>
+              <div class="message__data">
+                ${message.created_at}
+              </div>
+            </div>
+              <div class="message__text">
+              <div class="lower-message__content">
+                ${message.content}
+              </div>        
+          </div>
+          <img src=${message.image} >
+      </div>`
+      return html;
+      } else {
+      let html =
+      `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+              <div class="message__user__name">
+                ${message.user_name}
+              </div>
+              <div class="message__data">
+                ${message.created_at}
+              </div>
+          </div>
+              <div class="message__text">
+              <div class="lower-message__content">
+                ${message.content}
+              </div>        
+          </div>
+      </div>`
+      return html;
+      };
+    }
+  $('#new_message').on('submit', function(e){
+     e.preventDefault()
+     let formData = new FormData(this);
+     let url = $(this).attr('action');
+     $.ajax({
+       url: url,
+       type: "POST",
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+     })
+     .done(function(data){
+       let html = buildHTML(data);
+       $('.messages').append(html);
+       $('form')[0].reset();
+       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+       $(".form__submit").prop('disabled',false);
+     })
+     .fail(function(){
+       alert("メッセージ送信に失敗しました")
+     });
+     return false;
+  });
+});
