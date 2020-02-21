8943b8d6d418ddbb887b30df134d9fb2aa906f65
yasuhisa <yasuhisa3264@example.com>
2020-02-21 11:31:46 +0900

    komi

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -23,11 +23,13 @@ $(function() {
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
-    $(".js-add-user").append(html);
+    $("#group-member").append(html);
   }
   function addMember(userId) {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
+    console.log(html)
+    console.log($(`#${userId}`))
   }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
